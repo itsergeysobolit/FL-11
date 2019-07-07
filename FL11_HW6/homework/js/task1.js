434910cc3f18ebcf0a63fd5c9f735f4fc2f462f4
Serhii Sobol <itsergeysobol@gmail.com>
2019-07-07 20:58:54 +0300

    Done HW6

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,12 @@
-// Your code goes here
+let a1 = +prompt('Enter a1', '');
+let a2 = +prompt('Enter a2', '');
+let b1 = +prompt('Enter b1', '');
+let b2 = +prompt('Enter b2', '');
+let c1 = +prompt('Enter c1', '');
+let c2 = +prompt('Enter c2', '');
+const half = 2;
+if ((a1 + b1) / half === c1 && (a2 + b2) / half === c2) {
+    console.log('true');
+} else {
+    console.log('false');
+}
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,14 @@
-// Your code goes here
+let a = +prompt('Enter side length A', '');
+let b = +prompt('Enter side length B', '');
+let c = +prompt('Enter side length C', '');
+if (a < b + c && b < a + c && c < a + b) {
+    if (a === b && b === c) {
+        console.log('Equivalent triangle');
+    } else if (a === b || b === c || a === c) {
+        console.log('Isosceles triangle');
+    } else {
+        console.log('Normal triangle');
+    }
+} else {
+    console.log('Triangle doesn’t exist');
+}
