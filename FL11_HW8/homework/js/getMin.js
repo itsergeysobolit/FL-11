3d3d8271789514af85d65be0012cabf900a61596
Serhii Sobol <itsergeysobol@gmail.com>
2019-07-14 00:44:22 +0300

    Change task(3)

--- FL11_HW8/homework/js/getMin.js
+++ FL11_HW8/homework/js/getMin.js
@@ -1,2 +1,10 @@
-let getMin = (...arg) => Math.min(...arg);
-getMin(3, 0, -3); 
+function getMin() {
+    let numbers = arguments[0];
+    for (let i = 0; i < arguments.length; i++) {
+        if (numbers > arguments[i]) {
+            numbers = arguments[i];
+        }
+    }
+    return numbers;
+}
+getMin(3, 0, -3);
